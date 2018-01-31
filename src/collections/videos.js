06c768b0cb12beda5155d6c4fbf1bddbbaa3a75e
stephfunny <contactstephanief@gmail.com>
2018-01-30 20:17:37 -0800

    functioning get request from youtube api

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -6,8 +6,22 @@ var Videos = Backbone.Collection.extend({
     for (var i = 0; i < videos.length; i++) {
       this.models.push(new Video(videos[i]));
     }
-  }
+  },
 
+  search: function() {
+
+    var keyword = $('.form-control').val();
+    var url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${keyword}&type=video&key=${window.YOUTUBE_API_KEY}`;
+
+    $.ajax({
+      url: url,
+      success: function(data) {
+        console.log('success!', data);
+      },
+      dataType: 'json'
+    });
+
+  }
 
 
 });
